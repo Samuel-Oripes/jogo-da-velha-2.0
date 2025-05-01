5881e629b48205ecfae2cea072b7e1c5533c78a5
Samuel-Oripes <moreiraoripessamuelvictor@gmail.com>
2025-05-01 16:42:20 -0300

    Aprimorando o jogo da velha

---
+++ src/containers/Menu/index.jsx
@@ -0,0 +1,19 @@
+
+
+export function Menu() {
+
+    return (
+        <div>
+            <img src="./assets/logo.svg" alt="logo" />
+            <div>
+                <p>ESCOLHA O SÍMBOLO COM QUE DESEJA JOGAR</p>
+                <input type="radio" />
+                <p>LEMBRE-SE: X VAI PRIMEIRO</p>
+            </div>
+            <div>
+                <a>NOVO JOGO (VS CPU)</a>
+                <a>NOVO JOGO (2 JOGADORES)</a>
+            </div>
+        </div>
+    )
+}
