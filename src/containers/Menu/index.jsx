bde134c099a64297c9279e91266580a7bb90dd95
Samuel-Oripes <moreiraoripessamuelvictor@gmail.com>
2025-05-01 16:42:46 -0300

    formatações com Biome

--- src/containers/Menu/index.jsx
+++ src/containers/Menu/index.jsx
@@ -1,19 +1,18 @@
-
-
 export function Menu() {
-
-    return (
-        <div>
-            <img src="./assets/logo.svg" alt="logo" />
-            <div>
-                <p>ESCOLHA O SÍMBOLO COM QUE DESEJA JOGAR</p>
-                <input type="radio" />
-                <p>LEMBRE-SE: X VAI PRIMEIRO</p>
-            </div>
-            <div>
-                <a>NOVO JOGO (VS CPU)</a>
-                <a>NOVO JOGO (2 JOGADORES)</a>
-            </div>
-        </div>
-    )
+	return (
+		<div>
+			<img src="./assets/logo.svg" alt="logo" />
+			<div>
+				<p>ESCOLHA O SÍMBOLO COM QUE DESEJA JOGAR</p>
+				<input type="radio" />
+				<p>LEMBRE-SE: X VAI PRIMEIRO</p>
+			</div>
+			<div>
+				{/* biome-ignore lint/a11y/useValidAnchor: <explanation> */}
+				<a>NOVO JOGO (VS CPU)</a>
+				{/* biome-ignore lint/a11y/useValidAnchor: <explanation> */}
+				<a >NOVO JOGO (2 JOGADORES)</a>
+			</div>
+		</div>
+	);
 }
